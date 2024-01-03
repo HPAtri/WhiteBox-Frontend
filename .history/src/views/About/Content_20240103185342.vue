61b2a307150ea1fd3d778b12ba0bd3d888f95614
HPAtri <2479264057@qq.com>
2024-01-03 22:05:55 +0800

    submit

---
+++ .history/src/views/About/Content_20240103185342.vue
@@ -0,0 +1,204 @@
+<template>
+<div class="main">
+  <div class="article">
+    <h4 class="title">{{article[0].title}}</h4>
+    <div class="inf">
+      <!-- <span>作者：{{author}}</span>
+      <span>{{time}}</span>
+      <span>{{like}}喜欢</span> -->
+      <span>分类：{{classification}}</span>
+    </div>
+    <h6 class="content">{{content}}</h6>
+  </div>
+  <div class="release">
+
+  </div>
+  <div class="comment">
+
+
+  </div>
+  <!-- <img :src="banner" alt="" class="banner">
+  
+  <div class="main">
+    <div class="connect-game" v-if="gameName" @click="toGame">相关游戏：{{gameName}}</div>
+    <div class="title">{{art.title}}</div>
+    <div class="author">
+      <span>作者：{{art.author}}</span>
+      <span>{{time}}</span>
+      <span>{{like}}喜欢</span>
+      <span>分类：{{art.classify}}</span>
+    </div>
+    <div v-html="art.article">{{art.article}}</div>
+       <b-button :pressed="flag" pill variant="outline-danger" class="mb-2 like-btn" @click="likeIt">
+      <b-icon icon="heart" aria-hidden="true" class="heart-icon"></b-icon> {{like}}人喜欢
+    </b-button> -->
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      author:"",
+      like:0,
+      time:"",
+      banner:'',
+      flag:false,
+      likeState:true,//false时不再增加点赞数量了
+      id:'',
+      userId:"",
+      gameName:"",
+      title:"",
+      classification:"",
+      content:"",
+    }
+  },
+  methods:{
+    getContent(){
+      let id = this.$route.query.id
+        axios({
+        url:"http://192.168.137.44:10086/information/viewarticle",
+        method:'post',
+        headers:{
+        'accept': "application/json",
+    },
+        data:{
+        id:id
+        }})
+        .then(res=>{
+            this.article = res.data.data.articleEntity;
+            this.title = article[0].title
+            this.classification = article[0].classification
+            this.content = article[0].content
+        })
+    },
+    likeIt(){
+        this.flag = !this.flag
+        if(this.flag){
+          this.like =  this.like+1
+        }
+        if(this.flag==false){
+          this.like = this.like-1
+      }
+    },
+    toGame(){
+      this.$router.push({
+        path:"/game",
+        query:{game:this.gameName}
+      })
+    }
+  },
+  mounted(){
+    this.getContent()
+    // console.log(this.$route.query.id)
+    // let id = this.$route.query.id
+    // this.id = id
+    // let query = new this.$av.Query('information')
+    // query.get(id).then(res=>{
+    //   console.log("我是结果：",res)
+    //   if(res.attributes.gamename)
+    //   {this.gameName=res.attributes.gamename}
+    //   this.art = res.attributes
+    //   let date = res.createdAt
+    //   let y = date.getFullYear()
+    //   let m = date.getMonth() + 1//月份从0开始
+    //   m = m < 10 ? '0' + m : m
+    //   let d = date.getDate()
+    //   d = d < 10 ? ('0' + d) : d
+    //   this.time = y + '-' + m + '-' +d
+    //   this.banner = res.attributes.img.attributes.url
+    //   this.like = res.attributes.like//文章的点赞数
+    // })
+    // let query2 = new this.$av.Query("_User")
+    // let user = this.$av.User.current()
+    // let userid =user.id
+    // this.userId = userid
+    // let artId = this.id
+    // query2.get(userid).then(
+    //   todo=>{
+    //     console.log("这里",todo)
+    //     let likeList = todo.attributes.like
+    //     let res = likeList.indexOf(artId)
+    //     if (res!==-1){
+    //       this.flag = true
+    //       this.likeState = false
+    //       console.log("点过赞了吗",this.likeState)
+    //     }
+    //   })
+  },
+  // beforeDestroy(){
+  //   let query = new this.$av.Query("information")
+  //   let query2 = new this.$av.Query("_User")
+  //   let userId = this.userId
+  //   query.get(this.id).then((todo)=>{
+  //       if(this.flag&&this.likeState){
+  //       todo.increment("like",1)
+  //       todo.save().then(()=>{
+  //         query2.get(userId).then((res)=>{
+  //           res.add("like",this.id)
+  //           console.log("加入","this.id")
+  //           res.save()
+  //         })
+  //       })
+  //     }
+  //     if(!this.flag&&!this.likeState){
+  //       todo.increment("like",-1)
+  //        todo.save().then(()=>{
+  //         query2.get(userId).then((res)=>{
+  //           res.remove("like",this.id)
+  //           console.log("去掉",this.id)
+  //           res.save()
+  //         })
+  //       })
+  //     }
+  //   }) 
+  // }
+}
+</script>
+
+<style scoped> 
+.main{
+  margin-top:200px;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: flex-start;
+}
+.main{
+  padding: 40px;
+  background: #fff;
+  width: 800px;
+}
+.banner{
+  width: 800px;
+  height: 300px;
+  object-fit: cover;
+}
+.connect-game{
+  color:#fff;
+  background:cornflowerblue;
+}
+.title{
+  font-size: 36px;
+}
+.author{
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 20px;
+  margin-bottom: 30px;
+  color: gray;
+}
+.like-btn{
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  line-height: 1.5;
+}
+.heart-icon{
+  margin-right: 10px;
+}
+</style>
---
+++ .history/src/views/AddArticle_20240103165038.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="main">
+  <div class="m_container">
+      <h4 style="padding-bottom: 20px;">发布帖子</h4>
+    <div class="m_content">
+      <el-form ref="editForm" status-icon :model="editForm" :rules="rules1"  label-width="80px">
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="editForm.title"></el-input>
+        </el-form-item>
+        <el-form-item label="分类专栏" prop="type_id">
+          <el-select v-model="editForm.type" placeholder="请选择分类专栏">
+            <el-option v-for="(item,index) in getalltype" :key="index" :label="item.name" :value="item.id + ''"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="分类专栏" prop="type_id">
+        <el-select
+    v-model="editForm.type"
+    label="分类专栏"
+    multiple
+    filterable
+    remote
+    reserve-keyword
+    placeholder="请选择分类专栏"
+    :remote-method="remoteMethod"
+    :loading="loading">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select></el-form-item>
+  
+        <el-form-item label="发布板块" prop="flag">
+          <el-radio v-model="editForm.flag" label="2">杂谈</el-radio>
+          <el-radio v-model="editForm.flag" label="3">攻略</el-radio>
+        </el-form-item>
+        <el-form-item label="发布形式" prop="published">
+          <el-select v-model="editForm.published" placeholder="请选择发布形式，默认为私密">
+            <el-option label="私密" value="false"></el-option>
+            <el-option label="公开" value="true"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="内容" prop="content">
+          <mavon-editor
+              class="mavon"
+              ref="md"
+              @imgAdd="$imgAdd"
+              @imgDel="$imgDel"
+              :imageFilter="uploadBefore"
+              v-model="editForm.content"/>
+        </el-form-item>
+        <el-switch v-model="editForm.original" active-text="原创" inactive-text="转载">
+      </el-switch>
+        <el-form-item style="margin: auto; padding-top: 20px;">
+          <el-button type="primary" @click="submitForm()">保存发布</el-button>
+          <router-link to="/admin/allblogs">
+            <el-button style="margin-left: 10px;">返回</el-button>
+          </router-link>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+    name: "EditBlog",
+    data() {
+      return {
+        editForm: {
+          id: null,
+          title: '',
+          content: '',
+          type:'2',
+          flag:'杂谈',
+          original: true,
+          published: null,
+          options: [],
+          value: [],
+          list: [],
+          loading: false,
+          states: ["123","234"]
+        },
+        rules1: {  //表单校验
+          title: [
+            {required: true, message: '请输入标题', trigger: 'blur'},
+            {min: 3, max: 100, message: '长度在 3 到 100 个字符', trigger: 'blur'}
+          ],
+          content: [
+            {required: true, message: '请输入文章内容', trigger: 'blur'}
+          ],
+          type_id: [
+            {required: true, message: '分类专栏不能为空', trigger: 'blur'}
+          ],
+          flag: [
+            {required: true, message: '文章类型不能为空', trigger: 'blur'}
+          ],
+          published: [
+            {required: true, message: '发布形式不能为空', trigger: 'blur'}
+          ],
+        },
+        rules2:{ //表单校验
+          name: [
+            {required: true, message: '分类专栏名不能为空', trigger: 'blur'}
+          ],
+        },
+        formLabelWidth: '120px' ,
+        inputVisible: false,
+        inputValue: '',
+        btags: null,
+        tblogTags: [],
+      }
+    },
+    mounted() {
+      this.statesinit(),
+      this.list = this.states.map(item => {
+        return { value: `value:${item}`, label: `label:${item}` };
+      });
+    //   // 如果原始md字符串中存在曾上传的图片， 则需要将对应<img>中的src替换为base64
+    //   this.$nextTick(() => {
+    //     // $vm.$imgUpdateByUrl 详情见本页末尾
+    //     this.$refs.md.$imgUpdateByUrl(0, base64内容);
+    //   })
+    },
+    props:['id'],
+    methods: {
+
+      $imgAdd(pos, $file){
+        // 将图片上传到服务器
+        var formdata = new FormData();
+        formdata.append('image', $file);
+        axios({
+          url: 'http://192.168.137.44:10086/picture/save',
+          method: 'post',
+          headers: { 'Content-Type': 'multipart/form-data' },
+          data:formdata,
+        }).then((res) => {
+          // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+          // $vm.$img2Url 详情见本页末尾
+          this.$refs.md.$img2Url(pos, res.data.data.url);
+        })
+      },
+      statesinit(){
+        console.log(this.states);
+      },
+      remoteMethod(query) {
+        if (query !== '') {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false;
+            this.options = this.list.filter(item => {
+              return item.label.toLowerCase()
+                .indexOf(query.toLowerCase()) > -1;
+            });
+          }, 200);
+        } else {
+          this.options = [];
+        }
+      },
+      showInput() {
+        this.inputVisible = true;
+        this.$nextTick(
+          this.$refs.saveTagInput.$refs.input.focus()
+        );
+      },
+
+      submitForm() {
+        const token =  localStorage.getItem("token");
+        const userId =  localStorage.getItem("userId");
+        axios({
+        url:"http://192.168.137.44:10086/information/addarticle",
+        method:'post',
+        headers:{
+        'accept': "application/json",
+        'token':token,
+        },
+        data:{
+          userId: userId,
+          title: this.editForm.title,
+          content: this.editForm.content,
+          gameId:this.editForm.type,
+          classification:this.editForm.flag,
+          // original: this.editForm.original,
+          // published: this.editForm.published,
+        }})
+        .then(
+          alert("发布成功!")
+        );
+        },
+
+      }
+  }
+</script>
+
+<style>
+.main{
+  height: 800px;
+}
+.m_container{
+  padding-top:30px;
+  padding-bottom:25px;
+  padding-left:30px;
+  padding-right:30px;
+  margin: 100px auto;
+  width: 70%;
+  border-radius: 20px 20px 20px 20px;
+  background-color: white;
+  }
+.m_content{
+  padding-top:20px;
+  padding-left:60px;
+  padding-right:100px;
+  }
+</style>
